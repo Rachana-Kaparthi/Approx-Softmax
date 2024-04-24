9759ed30c784ab0689d17e11c25c2d721da20fcc
Lasya-G <140998582+Lasya-G@users.noreply.github.com>
2024-04-24 16:35:19 +0530

    Update softmax_8.sv

--- Verilog/softmax_8.sv
+++ Verilog/softmax_8.sv
@@ -21,7 +21,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module softmax_8 #(parameter wid_int = 5, wid_MSB1 = 4, wid_MSB2 = 0,wid_MSB3 = 0, wid_LSB1=0, wid_LSB2 = 4, wid_LSB3 = 4 )(input [16:0]x, output [20:0]exp);
+module softmax_8 #(parameter wid_int = 5, wid_MSB1 = 4, wid_MSB2 = 0,wid_MSB3 = 0, wid_LSB1=8)(input [16:0]x, output [20:0]exp);
 
 /// x range -10 to 10
 
